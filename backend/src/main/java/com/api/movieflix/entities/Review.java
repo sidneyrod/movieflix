6b694f8691897c237f1fcb974a5be70e2826e54c
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-08-12 15:12:55 -0300

    Entity Movie

--- backend/src/main/java/com/api/movieflix/entities/Review.java
+++ backend/src/main/java/com/api/movieflix/entities/Review.java
@@ -2,6 +2,7 @@ package com.api.movieflix.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class Review implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(columnDefinition = "TEXT")
 	private String text;
 
 	public Review() {
