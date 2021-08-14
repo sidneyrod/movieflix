a44de0bb504384d3e10c0b2012caaba1adb6acf2
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-08-14 12:38:03 -0300

    UserDTO

---
+++ backend/src/main/java/com/api/movieflix/dto/UserDTO.java
@@ -0,0 +1,68 @@
+package com.api.movieflix.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.api.movieflix.entities.User;
+
+public class UserDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	@NotBlank(message = "Campo obrigatório")
+	private String name;
+	
+	@Email(message = "Favor digitar um email válido")
+	private String email;
+	
+	Set<RoleDTO> roles = new HashSet<>();
+	
+	public UserDTO() {}
+
+	public UserDTO(Long id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	
+	public UserDTO(User user) {
+		id = user.getId();
+		name = user.getName();
+		email = user.getEmail();
+		user.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<RoleDTO> getRoles() {
+		return roles;
+	}
+}
