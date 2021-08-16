896b2d02527ba2b438b7090c0af3b586ecd2487c
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-08-16 19:48:23 -0300

    UserUpdateDTO, UserUpdateValid, UserUpdateDTO

---
+++ backend/src/main/java/com/api/movieflix/dto/UserUpdateDTO.java
@@ -0,0 +1,8 @@
+package com.api.movieflix.dto;
+
+import com.api.movieflix.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+}
