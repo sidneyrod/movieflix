e154dbdf1c20e0e723d704d0ee32e21ca0c61ebd
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-09-05 09:25:48 -0300

    Adding Home screen page

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,38 +1,40 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { CreateAccount, Login, MovieDetails, Movies } from '../screens';
+import { CreateAccount, Home, Login, MovieDetails, Movies } from '../screens';
+import colors from '../styles/colors';
+import { Text } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerTitle: 'MovieFlix',
-        headerStyle: { backgroundColor: '#FFC700' },
-        headerLeft: () => '',
-        headerRight: () => ''
-      }}
-    >
-      <Stack.Screen
-        name="Login"
-        component={ Login }
-      />
-
-      <Stack.Screen
-        name="CreateAccount"
-        component={ CreateAccount }
-      />
-
-      <Stack.Screen
-        name="Movies"
-        component={ Movies }
-      />
-
-      <Stack.Screen
-        name="MovieDetails"
-        component={ MovieDetails }
-      />
-    </Stack.Navigator>
-  )
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerTitle: '',
+                headerStyle: { backgroundColor: colors.yellow },
+                headerLeft: () => <Text>MovieFlix</Text>
+            }}
+        >
+            <Stack.Screen
+                name="Home"
+                component={Home}
+            />
+            <Stack.Screen
+                name="Login"
+                component={Login}
+            />
+            <Stack.Screen
+                name="CreateAccount"
+                component={CreateAccount}
+            />
+            <Stack.Screen
+                name="Movies"
+                component={Movies}
+            />
+            <Stack.Screen
+                name="MovieDetails"
+                component={MovieDetails}
+            />
+        </Stack.Navigator>
+    )
 }
--- mobile/src/styles/colors.ts
+++ mobile/src/styles/colors.ts
@@ -1,9 +1,11 @@
 export default {
-    yellow: '#FFC700',
-    darkGray: '#525252',
-    mediumGray: '#6C6C6C',
-    lightGray: '#9E9E9E',
-    black: '#000',
-    white: '#FFF',
-    whiteBorder: '#E1E1E1'
-  }
+  yellow: '#FFC700',
+  buttonImageContainer: 'rgba(38, 50, 56, 0.5)',
+  darkGray: '#525252',
+  mediumGray: '#6C6C6C',
+  lightGray: '#9E9E9E',
+  subtitle: '#F2F2F2',
+  black: '#000',
+  white: '#FFF',
+  whiteBorder: '#E1E1E1'
+}
